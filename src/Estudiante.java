d6388434348c6ef9e7878605f3049f0e679b73e0
andreycaro <andrey.caro108@gmail.com>
2023-11-25 23:29:12 -0500

    Nuevas Clases Estudiente, Curso y Gestor Academico

---
+++ src/Estudiante.java
@@ -0,0 +1,9 @@
+package src;
+
+public class Estudiante {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String fechaDeNacimiento;
+    private String estado;
+}
---
+++ src/GestorAcademico.java
@@ -0,0 +1,9 @@
+package src;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class GestorAcademico {
+    private ArrayList<Estudiante> estudiantes;
+    private ArrayList<Curso> cursos;
+    private HashMap<Integer, ArrayList<Estudiante>> estudiantesPorCurso;
+}
